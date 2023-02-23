bdd5735f1190458ba655b581babf08fa6f07c619
Juanma Barea Martinez <juanmabareamartinez@gmail.com>
2023-02-23 11:56:19 +0100

    Reduces configuration scope

--- scripts/setup_lab.sh
+++ scripts/setup_lab.sh
@@ -8,43 +8,32 @@ waitoperatorpod() {
 }
 
 #
+# Wait until pod $1 is ready
+#
+waitpodup(){
+  x=1
+  test=""
+  while [ -z "${test}" ]
+  do 
+    echo "Waiting ${x} times for pod ${1} in ns ${2}" $(( x++ ))
+    sleep 1 
+    test=$(oc get po -n ${2} | grep ${1})
+  done
+}
+
+#
 # Setup process
 #
 
-echo -n "Generate users credentials"
-
-htpasswd -c -b users.htpasswd admin admin
-htpasswd -b users.htpasswd developer developer
-
-echo -n "Creating htpasswd secrets in Openshift"
-
-oc delete secret lab-users -n openshift-config
-oc create secret generic lab-users --from-file=htpasswd=users.htpasswd -n openshift-config
-
-echo -n "Configuring OAuth to authenticate users via htpasswd"
-
-oc apply -f ./scripts/files/oauth.yaml
-
-echo -n "Disable self namespaces provisioner"
+echo -n "Disable self namespaces provisioner "
 
 oc patch clusterrolebinding.rbac self-provisioners -p '{"subjects": null}'
 
-echo -n "Creating Role Binding for admin user"
-
-oc adm policy add-cluster-role-to-user admin admin
-
-echo -n "Installing GitOps Operator"
+echo -n "Installing GitOps Operator "
 
 oc apply -f ./scripts/files/redhat_gitops.yaml
-echo -n "Waiting for GitOps Operators is ready..."
+echo -n "Waiting for GitOps Operators is ready... "
 waitoperatorpod gitops
 sleep 30
 
-echo -n "Installing Bitnami Sealed Secret"
-
-helm repo add bitnami-labs https://bitnami-labs.github.io/sealed-secrets/
-helm install gitops-sealed-secret bitnami-labs/sealed-secrets --version 2.7.1
-sleep 30
-
-echo -n "Configuring ArgoCD Service Account in order to create Bitnami Sealed Secret resources"
-oc apply -f ./scripts/files/sealed-secret-custom-role.yaml
+echo -n "Installation process has finished"
